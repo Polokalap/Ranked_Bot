5d21d5d5b83bb4431c504cb61f2cfee7ef1ac043
polokalap <Polokalap@autizm.us>
2026-06-03 19:30:52 +0200

    Ticket system

--- src/main/java/mel/Polokalap/Bot/Listeners/Ticket/OpenTicketCommandListener.java
+++ src/main/java/mel/Polokalap/Bot/Listeners/Ticket/OpenTicketCommandListener.java
@@ -1,14 +1,20 @@
 package mel.Polokalap.Bot.Listeners.Ticket;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import io.github.cdimascio.dotenv.Dotenv;
+import mel.Polokalap.Bot.Utils.CustomButton;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.channel.Channel;
 import net.dv8tion.jda.api.entities.channel.concrete.Category;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.components.buttons.ButtonStyle;
 
+import java.awt.*;
 import java.util.EnumSet;
 
 import static mel.Polokalap.Bot.Main.lang;
@@ -24,7 +30,21 @@ public class OpenTicketCommandListener extends ListenerAdapter {
 
         if (!event.getName().equals(name)) return;
 
+        // Check if channel already exists
+
         Guild guild = event.getGuild();
+        Category category = guild.getCategoryById(dotenv.get("TICKET_CATEGORY"));
+
+        for (Channel channel : category.getChannels()) {
+
+            if (channel.getName().replace(ticket.get("prefix").getAsString(), "").equals(event.getMember().getEffectiveName().toLowerCase())) {
+
+                event.reply(ticket.get("fail").getAsString()).setEphemeral(true).queue();
+                return;
+
+            }
+
+        }
 
         StringBuilder topicBuilder = new StringBuilder();
 
@@ -39,8 +59,54 @@ public class OpenTicketCommandListener extends ListenerAdapter {
 
         }
 
-        guild.createTextChannel(ticket.get("channel").getAsString().replace("%name%", event.getMember().getEffectiveName()))
-                .setParent(guild.getCategoryById(dotenv.get("TICKET_CATEGORY")))
+        // Building embed
+
+        EmbedBuilder embed = new EmbedBuilder();
+        StringBuilder description = new StringBuilder();
+        JsonArray colors = ticket.get("embed").getAsJsonObject().get("color").getAsJsonArray();
+
+        for (JsonElement line : ticket.get("embed").getAsJsonObject().get("description").getAsJsonArray()) {
+
+            description.append(
+                    line.getAsString()
+                            .replace("%user%", "<@" + event.getMember().getId() + ">")
+                    + "\n"
+            );
+
+        }
+
+        Color color = new Color(
+                colors.get(0).getAsInt(),
+                colors.get(1).getAsInt(),
+                colors.get(2).getAsInt()
+        );
+
+        // Creating buttons
+
+        JsonObject actions = ticket.get("embed").getAsJsonObject().get("actions").getAsJsonObject();
+
+        CustomButton closeButton = new CustomButton(
+                actions.getAsJsonObject().get("close").getAsJsonObject().get("text").getAsString(),
+                "ticket_close_" + event.getMember().getId(),
+                ButtonStyle.valueOf(actions.getAsJsonObject().get("close").getAsJsonObject().get("style").getAsString())
+        );
+
+        CustomButton archiveButton = new CustomButton(
+                actions.getAsJsonObject().get("archive").getAsJsonObject().get("text").getAsString(),
+                "ticket_archive_" + event.getMember().getId(),
+                ButtonStyle.valueOf(actions.getAsJsonObject().get("archive").getAsJsonObject().get("style").getAsString())
+        );
+
+        embed.setTitle(ticket.get("embed").getAsJsonObject().get("title").getAsString());
+        embed.setDescription(description.toString());
+        embed.setColor(color);
+        embed.setFooter(
+                ticket.get("embed").getAsJsonObject().get("footer").getAsString()
+                        .replace("%id%", event.getMember().getId())
+        );
+
+        guild.createTextChannel(ticket.get("prefix").getAsString() + event.getMember().getEffectiveName())
+                .setParent(category)
                 .addMemberPermissionOverride(
                         event.getMember().getIdLong(),
                         EnumSet.of(Permission.VIEW_CHANNEL, Permission.MESSAGE_SEND),
@@ -54,6 +120,15 @@ public class OpenTicketCommandListener extends ListenerAdapter {
                 .setTopic(topicBuilder.toString())
                 .queue(
                 channel -> {
+
+                    channel
+                            .sendMessage(ticket.get("embed").getAsJsonObject().get("ping").getAsString())
+                            .setEmbeds(embed.build())
+                            .setActionRow(
+                                    closeButton.getButton(),
+                                    archiveButton.getButton()
+                            )
+                            .queue();
 
                     event.reply(ticket.get("created").getAsString().replace("%channel%", "<#" + channel.getId() + ">")).setEphemeral(true).queue();
 
---
+++ src/main/java/mel/Polokalap/Bot/Listeners/Ticket/TicketButtonListener.java
@@ -0,0 +1,53 @@
+package mel.Polokalap.Bot.Listeners.Ticket;
+
+import com.google.gson.JsonObject;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.UserSnowflake;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.util.concurrent.TimeUnit;
+
+import static mel.Polokalap.Bot.Main.lang;
+
+public class TicketButtonListener extends ListenerAdapter {
+
+    @Override
+    public void onButtonInteraction(ButtonInteractionEvent event) {
+
+        String id = event.getComponentId();
+        JsonObject actions = lang
+                .get("commands").getAsJsonObject()
+                .get("ticket").getAsJsonObject()
+                .get("embed").getAsJsonObject()
+                .get("actions").getAsJsonObject();
+        JsonObject ticket = lang.get("commands").getAsJsonObject().get("ticket").getAsJsonObject();
+
+
+        if (id.startsWith("ticket_close_")) {
+
+            if (
+                    event.getChannel().getName().replace(ticket.get("prefix").getAsString(), "").equals(event.getMember().getEffectiveName().toLowerCase()) ||
+                    event.getMember().hasPermission(Permission.MESSAGE_MANAGE)
+            ) {
+
+                event.reply(actions.get("close").getAsJsonObject().get("done").getAsString()).queue(
+
+                        interactionHook -> {
+
+                            event.getChannel().delete().queueAfter(actions.get("close").getAsJsonObject().get("timeout").getAsInt(), TimeUnit.SECONDS);
+
+                        }
+
+                );
+                return;
+
+            }
+
+            event.reply(actions.get("permissions").getAsString()).setEphemeral(true).queue();
+
+        }
+
+    }
+
+}
