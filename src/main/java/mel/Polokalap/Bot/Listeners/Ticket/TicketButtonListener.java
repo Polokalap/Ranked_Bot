bf6bfe38d8ac4db2d6f1e1344440631dd31b5fd1
polokalap <Polokalap@autizm.us>
2026-06-03 20:26:46 +0200

    Added archive option to tickets

--- src/main/java/mel/Polokalap/Bot/Listeners/Ticket/TicketButtonListener.java
+++ src/main/java/mel/Polokalap/Bot/Listeners/Ticket/TicketButtonListener.java
@@ -1,10 +1,13 @@
 package mel.Polokalap.Bot.Listeners.Ticket;
 
 import com.google.gson.JsonObject;
+import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.UserSnowflake;
+import net.dv8tion.jda.api.entities.channel.middleman.GuildChannel;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.managers.channel.concrete.TextChannelManager;
 
 import java.util.concurrent.TimeUnit;
 
@@ -22,7 +25,7 @@ public class TicketButtonListener extends ListenerAdapter {
                 .get("embed").getAsJsonObject()
                 .get("actions").getAsJsonObject();
         JsonObject ticket = lang.get("commands").getAsJsonObject().get("ticket").getAsJsonObject();
-
+        Dotenv dotenv = Dotenv.load();
 
         if (id.startsWith("ticket_close_")) {
 
@@ -35,7 +38,54 @@ public class TicketButtonListener extends ListenerAdapter {
 
                         interactionHook -> {
 
-                            event.getChannel().delete().queueAfter(actions.get("close").getAsJsonObject().get("timeout").getAsInt(), TimeUnit.SECONDS);
+                            event.getChannel()
+                                    .delete()
+                                    .queueAfter(
+                                            actions.get("close")
+                                                    .getAsJsonObject()
+                                                    .get("timeout").getAsInt(), TimeUnit.SECONDS
+                                    );
+
+                        }
+
+                );
+                return;
+
+            }
+
+            event.reply(actions.get("permission").getAsString()).setEphemeral(true).queue();
+
+        }
+
+        if (id.startsWith("ticket_archive_")) {
+
+            if (event.getMember().hasPermission(Permission.MESSAGE_MANAGE)) {
+
+                event.reply(actions.get("archive").getAsJsonObject().get("done").getAsString().replace("%user%", "<@" + event.getMember().getId() + ">")).queue(
+
+                        interactionHook -> {
+
+                            GuildChannel channel = (GuildChannel) event.getChannel();
+                            String newName = event.getChannel().getName().replace(ticket.get("prefix").getAsString(), ticket.get("archived-prefix").getAsString());
+
+                            ((TextChannelManager) channel.getManager())
+                                    .setParent(event.getGuild().getCategoryById(dotenv.get("ARCHIVED_CATEGORY")))
+                                    .setName(newName)
+                                    .queue();
+
+                            channel.getPermissionContainer().getPermissionOverrides().forEach(override -> {
+                                override.delete().queue();
+                            });
+
+                            channel.getPermissionContainer()
+                                    .upsertPermissionOverride(event.getGuild().getPublicRole())
+                                    .deny(Permission.VIEW_CHANNEL)
+                                    .queueAfter(
+                                            actions.get("archive")
+                                                    .getAsJsonObject()
+                                                    .get("timeout").getAsInt(),
+                                            TimeUnit.SECONDS
+                                    );
 
                         }
 
